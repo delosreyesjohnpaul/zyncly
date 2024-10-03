3314380cba19df06c15cc26e97bede6952492e8c
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-03 16:30:09 +0800

    fin empty state

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,43 @@
-import { redirect } from "next/navigation";
+import { notFound } from "next/navigation";
+import prisma from "../lib/db";
 import { requireUser } from "../lib/hooks";
+import { EmptyState } from "../components/EmptyState";
+
+async function getData(id: string) {
+    const data = await prisma.user.findUnique({
+        where: {
+            id: id,
+          },
+      
+          select: {
+            eventType: {
+              select: {
+                id: true,
+                active: true,
+                title: true,
+                url: true,
+                duration: true,
+              },
+            },
+          },
+    });
+    if (!data) {
+        return notFound();
+      }
+    
+      return data;
+}
 
 export default async function DashboardPage() {
     const session = await requireUser();
+    const data = await getData(session.user?.id as string);
     return (
-        <div>
-            <h1>Hello from  the dashboard</h1>
-        </div>
+        <>
+            {data.eventType.length === 0 ? (
+                <EmptyState title="You have no Event Types right now" description="You can create your first event type by clicking the button bellow" buttonText="Add event type" href="/dashboard/new" />
+            ): (
+                <p>We have event type</p>
+            )}
+        </>
     )
 }
