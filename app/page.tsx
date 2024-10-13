db39139c92fa8ac1ae0c2206322b18160a889a42
Shaina Heart <delosreyesjohnpaul30@gmail.com>
2024-10-13 16:24:23 +0800

    hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { redirect } from "next/navigation";
 import { Navbar } from "./components/Navbar";
 import { auth } from "./lib/auth";
+import { Hero } from "./components/Hero";
 
 export default async function Home() {
   const session = await auth();
@@ -12,6 +13,7 @@ export default async function Home() {
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <Navbar/>
+      <Hero/>
     </div>
   );
 }
